20b48e798d26e5d46d75b994ff0a6935ccec5343
Åsa Jonsson <Asa.Jonsson@lexicon.se>
2022-11-16 14:12:05 +0100

    Add project files.

---
+++ PeopleApp/wwwroot/js/AjaxJson.js
@@ -0,0 +1,15 @@
+﻿function ajaxPeopleList(actionUrl) {
+    $.get(actionUrl, function (response) {
+        document.getElementById("result").innerHTML = response;
+    });
+}
+
+function ajaxPost(actionUrl, inputId) {
+    let inputElement = $("#" + inputId);
+    var data = {[inputElement.attr("name")]:inputElement.val()
+
+    }
+    $.post(actionUrl, data, function (response) {
+        document.getElementById("result").innerHTML = response;
+    })
+}
